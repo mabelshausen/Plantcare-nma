8881f204eb2290c605a974a2b686c497ca4c2448
marijn.abelshausen <marijn.abelshausen@student.howest.be>
2021-05-15 16:08:18 +0200

    Add Plant and Room models

---
+++ app/src/main/java/be/howest/marijnabelshausen/plantcare/domain/Plant.kt
@@ -0,0 +1,8 @@
+package be.howest.marijnabelshausen.plantcare.domain
+
+data class Plant (
+    val id: Int,
+    val name: String,
+    val sciName: String,
+    val age: Int
+)
---
+++ app/src/main/java/be/howest/marijnabelshausen/plantcare/domain/Room.kt
@@ -0,0 +1,6 @@
+package be.howest.marijnabelshausen.plantcare.domain
+
+data class Room (
+    val int: Int,
+    val name: String
+)
