49d12eb6cbb06991fcce038b165ac497ea9c042f
marijn.abelshausen <marijn.abelshausen@student.howest.be>
2021-05-15 17:19:57 +0200

    Prepare for GridView

--- app/src/main/java/be/howest/marijnabelshausen/plantcare/water/WaterViewModel.kt
+++ app/src/main/java/be/howest/marijnabelshausen/plantcare/water/WaterViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import be.howest.marijnabelshausen.plantcare.domain.Plant
 import be.howest.marijnabelshausen.plantcare.network.PlantCareApi
 import kotlinx.coroutines.launch
 import java.lang.Exception
@@ -15,6 +16,11 @@ class WaterViewModel : ViewModel() {
     val response: LiveData<String>
         get() = _response
 
+    private val _plants = MutableLiveData<List<Plant>>()
+
+    val plants: LiveData<List<Plant>>
+        get() = _plants
+
     init {
         getPlants()
     }
@@ -22,8 +28,8 @@ class WaterViewModel : ViewModel() {
     private fun getPlants() {
         viewModelScope.launch {
             try {
-                val listResult = PlantCareApi.retrofitService.getPlants()
-                _response.value = "Success: ${listResult.size} Plants retrieved"
+                _plants.value = PlantCareApi.retrofitService.getPlants()
+                _response.value = "Success: Plants retrieved"
             } catch (e: Exception) {
                 _response.value = "Failure: " + e.message
             }
