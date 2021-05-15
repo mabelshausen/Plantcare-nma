8e5715853d238ce26e78cb66eec753b46d76662e
marijn.abelshausen <marijn.abelshausen@student.howest.be>
2021-05-15 16:19:03 +0200

    Implement Moshi converter

--- app/src/main/java/be/howest/marijnabelshausen/plantcare/network/PlantCareApiService.kt
+++ app/src/main/java/be/howest/marijnabelshausen/plantcare/network/PlantCareApiService.kt
@@ -1,22 +1,29 @@
 package be.howest.marijnabelshausen.plantcare.network
 
+import be.howest.marijnabelshausen.plantcare.domain.Room
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Call
 import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
 private const val BASE_URL = "http://192.168.0.172:8000/api/"
 //private const val BASE_URL = "http://192.168.10.10/api/"
 
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
 private val retrofit = Retrofit.Builder()
-    .addConverterFactory(ScalarsConverterFactory.create())
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(BASE_URL)
     .build()
 
 interface PlantCareApiService {
     @GET("rooms")
     fun getRooms():
-            Call<String>
+            Call<List<Room>>
 }
 
 object PlantCareApi {
--- app/src/main/java/be/howest/marijnabelshausen/plantcare/water/WaterViewModel.kt
+++ app/src/main/java/be/howest/marijnabelshausen/plantcare/water/WaterViewModel.kt
@@ -3,6 +3,7 @@ package be.howest.marijnabelshausen.plantcare.water
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import be.howest.marijnabelshausen.plantcare.domain.Room
 import be.howest.marijnabelshausen.plantcare.network.PlantCareApi
 import retrofit2.Call
 import retrofit2.Callback
@@ -21,12 +22,12 @@ class WaterViewModel : ViewModel() {
 
     private fun getRooms() {
         PlantCareApi.retrofitService.getRooms().enqueue(
-            object : Callback<String> {
-                override fun onResponse(call: Call<String>, response: Response<String>) {
-                    _response.value = response.body()
+            object : Callback<List<Room>> {
+                override fun onResponse(call: Call<List<Room>>, response: Response<List<Room>>) {
+                    _response.value = "Success: ${response.body()?.size} Rooms retrieved"
                 }
 
-                override fun onFailure(call: Call<String>, t: Throwable) {
+                override fun onFailure(call: Call<List<Room>>, t: Throwable) {
                     _response.value = "Failure: " + t.message
                 }
             })
