8bdaa200aeb86b0d4bc29ead4fe550c5acd006d4
marijn.abelshausen <marijn.abelshausen@student.howest.be>
2021-05-15 14:55:41 +0200

    Add PlantCareApiService

--- app/src/main/java/be/howest/marijnabelshausen/plantcare/network/PlantCareApiService.kt
+++ app/src/main/java/be/howest/marijnabelshausen/plantcare/network/PlantCareApiService.kt
@@ -1,4 +1,26 @@
 package be.howest.marijnabelshausen.plantcare.network
 
-class PlantCareApiService {
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+
+private const val BASE_URL = "http://192.168.0.172:8000/api/"
+//private const val BASE_URL = "http://192.168.10.10/api/"
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(ScalarsConverterFactory.create())
+    .baseUrl(BASE_URL)
+    .build()
+
+interface PlantCareApiService {
+    @GET("rooms")
+    fun getRooms():
+            Call<String>
+}
+
+object PlantCareApi {
+    val retrofitService : PlantCareApiService by lazy {
+        retrofit.create(PlantCareApiService::class.java)
+    }
 }
