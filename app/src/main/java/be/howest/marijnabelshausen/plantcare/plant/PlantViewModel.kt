c6a78c8abef6628c2156f4f5076d912c8d19b5a3
marijn.abelshausen <marijn.abelshausen@student.howest.be>
2021-05-16 15:37:32 +0200

    Implement PlantViewModel

--- app/src/main/java/be/howest/marijnabelshausen/plantcare/plant/PlantViewModel.kt
+++ app/src/main/java/be/howest/marijnabelshausen/plantcare/plant/PlantViewModel.kt
@@ -1,7 +1,32 @@
 package be.howest.marijnabelshausen.plantcare.plant
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import be.howest.marijnabelshausen.plantcare.domain.Plant
+import be.howest.marijnabelshausen.plantcare.network.PlantCareApi
+import kotlinx.coroutines.launch
+import java.lang.Exception
 
 class PlantViewModel(private val plantId: Int = 0) : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private val _plant = MutableLiveData<Plant>()
+
+    val plant : LiveData<Plant>
+        get() = _plant
+
+    init {
+        getPlant()
+    }
+
+    private fun getPlant() {
+        viewModelScope.launch {
+            try {
+                _plant.value = PlantCareApi.retrofitService.getPlantById(plantId)
+            } catch (e: Exception) {
+                //TODO
+            }
+        }
+    }
 }
