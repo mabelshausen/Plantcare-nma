5360f243793a6025b251ecdc0cb7eefa55fddbe6
marijn.abelshausen <marijn.abelshausen@student.howest.be>
2021-07-14 10:17:34 +0200

    Implement Rooms page

--- app/src/main/java/be/howest/marijnabelshausen/plantcare/rooms/RoomsFragment.kt
+++ app/src/main/java/be/howest/marijnabelshausen/plantcare/rooms/RoomsFragment.kt
@@ -6,7 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import be.howest.marijnabelshausen.plantcare.databinding.RoomsFragmentBinding
+import be.howest.marijnabelshausen.plantcare.domain.Room
 
 class RoomsFragment : Fragment() {
 
@@ -25,6 +27,17 @@ class RoomsFragment : Fragment() {
         val binding = RoomsFragmentBinding.inflate(inflater)
         binding.setLifecycleOwner(this)
         binding.viewModel = viewModel
+
+        val adapter = RoomAdapter(RoomListener { roomId ->
+            viewModel.onRoomClicked(roomId)
+        })
+        binding.roomList.adapter = adapter
+
+        viewModel.rooms.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                adapter.data = it
+            }
+        })
 
         return binding.root
     }
--- app/src/main/java/be/howest/marijnabelshausen/plantcare/rooms/RoomsViewModel.kt
+++ app/src/main/java/be/howest/marijnabelshausen/plantcare/rooms/RoomsViewModel.kt
@@ -1,7 +1,45 @@
 package be.howest.marijnabelshausen.plantcare.rooms
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import be.howest.marijnabelshausen.plantcare.domain.Room
+import be.howest.marijnabelshausen.plantcare.network.PlantCareApi
+import kotlinx.coroutines.launch
 
 class RoomsViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+
+    private val _rooms = MutableLiveData<List<Room>>()
+
+    val rooms: LiveData<List<Room>>
+        get() = _rooms
+
+    private val _navigateToRoom = MutableLiveData<Int?>()
+
+    val navigateToRoom
+        get() = _navigateToRoom
+
+    init {
+        getRooms()
+    }
+
+    private fun getRooms() {
+        viewModelScope.launch {
+            try {
+                _rooms.value = PlantCareApi.retrofitService.getRooms()
+            } catch (e: Exception) {
+                // Do something
+            }
+        }
+    }
+
+    fun onRoomClicked(id: Int) {
+        _navigateToRoom.value = id
+    }
+
+    fun onRoomNavigated() {
+        _navigateToRoom.value = null
+    }
 }
