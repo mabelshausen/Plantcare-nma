afd937023f640485972a383aad4a79f0b03c5e3c
marijn.abelshausen <marijn.abelshausen@student.howest.be>
2021-07-30 11:11:26 +0200

    Fill PlantForm

--- app/src/main/java/be/howest/marijnabelshausen/plantcare/plant/PlantFormViewModel.kt
+++ app/src/main/java/be/howest/marijnabelshausen/plantcare/plant/PlantFormViewModel.kt
@@ -1,10 +1,42 @@
 package be.howest.marijnabelshausen.plantcare.plant
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import be.howest.marijnabelshausen.plantcare.domain.Plant
 
-class PlantFormViewModel : ViewModel() {
+class PlantFormViewModel(private val plantId: Int) : ViewModel() {
 
-    init {}
+    private var isEdit = (plantId != 0)
+
+    private val _plant = MutableLiveData<Plant>()
+
+    val name = MutableLiveData<String>()
+    val sciName = MutableLiveData<String>()
+    val age = MutableLiveData<String>()
+    val waterFreq = MutableLiveData<String>()
+
+    private val _navigateToPlants = MutableLiveData<Int?>()
+    val navigateToPlants
+        get() = _navigateToPlants
+
+    private val _navigateToPlantDetail = MutableLiveData<Int?>()
+    val navigateToPlantDetail
+        get() = _navigateToPlantDetail
+
+    init {
+        if (isEdit) {
+            getPlant()
+        } else {
+            name.value = "Plant Name"
+            sciName.value = "Scientific Name"
+            age.value = "Age (in years)"
+            waterFreq.value = "Days between watering"
+        }
+    }
+
+    private fun getPlant() {
+        TODO("Not yet implemented")
+    }
 
     fun onSaveButtonClicked() {
         TODO("Not yet implemented")
