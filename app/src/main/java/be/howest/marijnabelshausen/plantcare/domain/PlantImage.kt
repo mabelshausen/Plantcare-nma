3bc14bb1008318ddcbad4ad4d1c5092bcfec68de
marijn.abelshausen <marijn.abelshausen@student.howest.be>
2021-08-04 11:37:04 +0200

    Add PlantImage entity

--- app/src/main/java/be/howest/marijnabelshausen/plantcare/domain/PlantImage.kt
+++ app/src/main/java/be/howest/marijnabelshausen/plantcare/domain/PlantImage.kt
@@ -1,4 +1,10 @@
 package be.howest.marijnabelshausen.plantcare.domain
 
-class PlantImage {
-}
+import androidx.room.Entity
+
+@Entity(tableName = "plant_image")
+data class PlantImage (
+    val plantId: Int,
+    val imagePath: String,
+    val timeStamp: String
+)
