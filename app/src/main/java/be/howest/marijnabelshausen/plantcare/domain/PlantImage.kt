faecf59fc56c93d5986d61b709f3c353a379e0ee
marijn.abelshausen <marijn.abelshausen@student.howest.be>
2021-08-04 11:40:32 +0200

    Add column annotations

--- app/src/main/java/be/howest/marijnabelshausen/plantcare/domain/PlantImage.kt
+++ app/src/main/java/be/howest/marijnabelshausen/plantcare/domain/PlantImage.kt
@@ -1,10 +1,14 @@
 package be.howest.marijnabelshausen.plantcare.domain
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 
-@Entity(tableName = "plant_image")
+@Entity(tableName = "plant_image", primaryKeys = ["plant_id", "image_path"])
 data class PlantImage (
+    @ColumnInfo(name = "plant_id")
     val plantId: Int,
+    @ColumnInfo(name = "image_path")
     val imagePath: String,
+    @ColumnInfo(name = "timestamp")
     val timeStamp: String
 )
